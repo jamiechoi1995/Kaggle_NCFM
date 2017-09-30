134947eaac073e2edf05b6a41dcd63a69266a3fa
jamiechoi1995 <no.23waterfish@163.com>
2017-09-30 17:58:10 +0800

    Modified train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from keras.applications.inception_v3 import InceptionV3
 import os
 from keras.layers import Flatten, Dense, AveragePooling2D
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.optimizers import RMSprop, SGD
 from keras.callbacks import ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
@@ -9,15 +9,17 @@ from keras.preprocessing.image import ImageDataGenerator
 learning_rate = 0.0001
 img_width = 299
 img_height = 299
-nbr_train_samples = 3019
-nbr_validation_samples = 758
+nbr_train_samples = 53879
+nbr_validation_samples = 7120
 nbr_epochs = 25
 batch_size = 32
 
-train_data_dir = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/train_split'
-val_data_dir = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/val_split'
+train_data_dir = '/media/wcai/8844289a-3afc-4ca9-98c0-a29abdb55c48/tdtd/wcai/ai_challenger_scene_train_20170904/scene_train_images_20170904'
+val_data_dir = '/media/wcai/8844289a-3afc-4ca9-98c0-a29abdb55c48/tdtd/wcai/ai_challenger_scene_validation_20170908/scene_validation_images_20170908'
 
-FishNames = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
+FishNames = []
+for num in range(0,80):
+        FishNames.append(str(num))
 
 print('Loading InceptionV3 Weights ...')
 InceptionV3_notop = InceptionV3(include_top=False, weights='imagenet',
@@ -29,9 +31,10 @@ print('Adding Average Pooling Layer and Softmax Output Layer ...')
 output = InceptionV3_notop.get_layer(index = -1).output  # Shape: (8, 8, 2048)
 output = AveragePooling2D((8, 8), strides=(8, 8), name='avg_pool')(output)
 output = Flatten(name='flatten')(output)
-output = Dense(8, activation='softmax', name='predictions')(output)
+output = Dense(80, activation='softmax', name='predictions')(output)
 
 InceptionV3_model = Model(InceptionV3_notop.input, output)
+# InceptionV3_model = load_model('weights.h5') #load model to continue your training
 #InceptionV3_model.summary()
 
 optimizer = SGD(lr = learning_rate, momentum = 0.9, decay = 0.0, nesterov = True)
@@ -77,8 +80,8 @@ validation_generator = val_datagen.flow_from_directory(
 
 InceptionV3_model.fit_generator(
         train_generator,
-        samples_per_epoch = nbr_train_samples,
+        steps_per_epoch = nbr_train_samples/batch_size,
         nb_epoch = nbr_epochs,
         validation_data = validation_generator,
-        nb_val_samples = nbr_validation_samples,
+        validation_steps = nbr_validation_samples/batch_size,
         callbacks = [best_model])
